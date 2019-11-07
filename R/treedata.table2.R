680d8bbcb967dfcdcf92131ec0108950599203b6
cromanpa94 <cromanpa94@gmail.com>
2019-11-07 17:56:09 -0500

    treedata.table2 updated

--- R/treedata.table2.R
+++ R/treedata.table2.R
@@ -2,8 +2,22 @@
   .dat <- x$dat
   .dat[,rowid := seq_len(nrow(.dat))]
   .dat <- .dat[..., by=rowid]
-  .phy <- drop.tip(x$phy, which(!1:nrow(x$dat) %in% .matched[,rowid]))
+  .phy <- drop.tip(x$phy, which(!1:nrow(x$dat) %in% .dat[,rowid]))
   x$phy <- .phy
   x$dat <- .dat
   return(x)
 }
+
+
+##Examples
+library(data.table)
+library(treeplyr)
+data(anolis)
+td <- make.treedata(anolis$phy, anolis$dat)
+td$dat <- data.table(td$dat)
+class(td) <- c("treedata.table", "list")
+
+td[,SVL]
+td[island == "Cuba" & ecomorph == "TG", .(ecomorph, island, SVL)]
+td[island == "Cuba" & ecomorph == "TG",]
+
