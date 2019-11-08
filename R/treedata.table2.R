56f9d56807e844f48ea6daad635d5f1097f38a59
cromanpa94 <cromanpa94@gmail.com>
2019-11-07 19:39:49 -0500

    new version treedata.table2

--- R/treedata.table2.R
+++ R/treedata.table2.R
@@ -2,17 +2,14 @@
   .dat <- x$dat
   .dat[,rowid := seq_len(nrow(.dat))]
   dots <- lazyeval::lazy_dots(...)
-  if(length(dots)>1){
-    if(nchar(dots[[2]]$expr)!=0){
+    if(nchar(dots[[2]]$expr)[1]!=0){
       .dat <- .dat[..., by=rowid] #Column select so need to preserve rowid
     } else{dat <- .dat[...]}
-  } else {dat <- .dat[...]}
   .phy <- drop.tip(x$phy, which(!1:nrow(x$dat) %in% .dat[,rowid]))
   x$phy <- .phy
   x$dat <- .dat[,!"rowid"]
   return(x)
 }
-
 
 ##Examples
 library(data.table)
@@ -24,8 +21,7 @@ class(td) <- c("treedata.table", "list")
 
 td[,SVL]
 td[island == "Cuba" & ecomorph == "TG", .(ecomorph, island, SVL)]
-tmp <- td[island == "Hispaniola",]
-td[order(island)]
+td[island == "Hispaniola",]
 
 td[island == "Cuba" & ecomorph == "TG",]
 
