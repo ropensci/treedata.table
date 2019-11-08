bb3a338ff61f5e36247f95adb64c4115c707347b
cromanpa94 <cromanpa94@gmail.com>
2019-11-08 11:02:20 -0500

    tdt

--- R/tdt.R
+++ R/tdt.R
@@ -1,8 +1,21 @@
-treedata.table <- function(tdObject, ...){
-  UseMethod("treedply")
-}
+#' Run a function on a \code{treedata.table} object
+#'
+#' @param tdObject A treedata.table object
+#' @param ... A function call.
+#'
+#' @details This function allows R functions that use trees and data to be run on
+#' \code{treedata.table} objects.
+#'
+#' @return Function output
+#'
+#' @examples
+#' data(anolis)
+#' td <- as.treedata.table(anolis$phy, anolis$dat)
+#' t.dt(td, geiger::fitContinuous(phy, extractVector(td, SVL), model="BM", ncores=1))
+#' tdt(td, phytools::phenogram(phy, extractVector(td, SVL), quiet=TRUE, spread.labels=FALSE))
+#' @export
 
-treedata.table <- function(tdObject, ...){
+tdt <- function(tdObject, ...){
   if(!is.call(substitute(...))){
     call <- list(...)[[1]]
   } else {
@@ -18,30 +31,3 @@ treedata.table <- function(tdObject, ...){
     return(out)
   }
 }
-
-
-extractVector <- function(td, ...){
-  dat <- td$dat
-  args <- as.character(substitute(list(...)))[-1L]
-  arg_sub <- type.convert(args)
-  if(is.numeric(arg_sub) | is.integer(arg_sub)) args <- arg_sub
-  vecs <- lapply(args,function(x) dat[[x]])
-  vecs <- lapply(vecs, function(x) setNames(x, td$phy$tip.label))
-  if(length(vecs)==1){
-    vecs = vecs[[1]]
-  } else {names(vecs) <- args}
-  return(vecs)
-}
-
-
-treedata.table(td, geiger::fitContinuous(phy, extractVector(td, SVL), model="BM", ncores=1))
-treedata.table(td, geiger::fitContinuous(phy, extractVector(td, awesomeness), model="BM", ncores=1))
-treedata.table(td, phytools::phenogram(phy, extractVector(td, SVL), quiet=TRUE, spread.labels=FALSE))
-treedata.table(td, list("K" = phytools::phylosig(phy, extractVector(td, SVL), "K"),
-                  "lambda" = phytools::phylosig(phy, extractVector(td, SVL), "lambda")))
-
-treedata.table(td, geiger::rescale, model="OU", 10) ##I'm still wotking on this one
-
-
-
-
