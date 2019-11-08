9bd56a386eec725e86f3b6209d5372cb03f40f62
uyedaj <juyeda@vt.edu>
2019-11-08 16:21:54 -0500

    adding name detect option for matching

--- R/as.treedata.table.R
+++ R/as.treedata.table.R
@@ -17,17 +17,40 @@
 #' td <- as.treedata.table(anolis$phy, anolis$dat)
 #' @export
 
-as.treedata.table<-function(tree, data){
+as.treedata.table<-function(tree, data, name_column="detect"){
   if(class(tree) != "phylo"){
     stop("Please use a class 'phylo' tree \n")
     }
-  if(class(data) != "data.frame"){
-    stop("Your data MUST be of class data.frame")
+  #if(class(data) != "data.frame"){
+  #  stop("Your data MUST be of class data.frame")
+  #  }
+  #if(dim(data)[2] < 2){
+  #  stop("Your data MUST have at least two columns (tip.names; nstates)")
+  #  }
+  if(is.vector(data)){
+    data <- data.frame(as.matrix(data))
+    colnames(data) <- "trait"
+  }
+  if(is.null(colnames(data))){
+    colnames(data) <- paste("trait", 1:ncol(data), sep="")
+  }
+  coln <- colnames(data)
+  if(name_column=="detect"){
+    if(is.null(rownames(data))){
+      tmp.df <- data.frame(data)
+      offset <- 0
+    } else {
+      tmp.df <- data.frame(rownames(data), data)
+      offset <- 1
     }
-  if(dim(data)[2] < 2){
-    stop("Your data MUST have at least two columns (tip.names; nstates)")
+    matches <- sapply(tmp.df, function(x) sum(x %in% tree$tip.label))
+    if(all(matches==0)) stop("No matching names found between data and tree")
+    name_column <- which(matches==max(matches))-offset
+  } else{
+    if(is.character(name_column)){
+      name_column <- which(name_column==coln)[1]
     }
-
+  }
   if(geiger::name.check(tree, data.names = data[,1] )[1] != "OK"){
     data_not_tree <- setdiff(as.character(data[,1]), tree$tip.label)
     tree_not_data <- setdiff(tree$tip.label, data[,1])
@@ -36,7 +59,8 @@ as.treedata.table<-function(tree, data){
     data_not_tree <- "OK"
     tree_not_data <- "OK"
   }
-  data <- data[match(tree$tip.label, data$X),]
+  data <- data[match(tree$tip.label, data[,name_column]),]
+  colnames(data)[name_column] <- "tip.label"
   comb<-list(phy=tree, dat=data.table::as.data.table(data))
   attr(comb,'data_not_tree') <- data_not_tree
   attr(comb,'tree_not_data') <- tree_not_data
