dc2c068387a992ffd85c765e0a7572b73a51262d
Cristian Román <cromanpa94@gmail.com>
2019-12-13 10:07:36 -0700

    tdt updated to handle multiPhylo objects

--- R/tdt.R
+++ R/tdt.R
@@ -6,14 +6,25 @@
 #' @details This function allows R functions that use trees and data to be run on
 #' \code{treedata.table} objects.
 #'
-#' @return Function output
+#' @return Function output for a single tree (phylo) or a list of function outputs (multiPhylo)
 #'
 #' @examples
 #' data(anolis)
-#' td <- as.treedata.table(anolis$phy, anolis$dat)
 #' \dontrun{
+#'
+#' A treedata.table object with a phylo $phy
+#' td <- as.treedata.table(anolis$phy, anolis$dat)
 #' tdt(td, geiger::fitContinuous(phy, extractVector(td, SVL), model="BM", ncores=1))
 #' tdt(td, phytools::phenogram(phy, extractVector(td, SVL), quiet=TRUE, spread.labels=FALSE))
+#'
+#'
+#' A treedata.table object with a multiPhylo $phy
+#' treesFM<-list(anolis$phy,anolis$phy)
+#' class(treesFM) <- "multiPhylo"
+#' td <- as.treedata.table(treesFM, anolis$dat)
+#' tdt(td, geiger::fitContinuous(phy, extractVector(td, SVL), model="BM", ncores=1))
+#' tdt(td, phytools::phenogram(phy, extractVector(td, SVL), quiet=TRUE, spread.labels=FALSE))
+#'
 #' }
 #' @export
 
@@ -24,12 +35,26 @@ tdt <- function(tdObject, ...){
     call <- substitute(...)
   }
   env <- new.env(parent = parent.frame(), size = 1L)
-  env$phy <- tdObject$phy
   env$dat <- tdObject$dat
-  out <- eval(call, env)
-  if(is.null(out)){
-    invisible()
-  } else {
-    return(out)
+
+  if(class(tdObject$phy) == "phylo"){
+    env$phy <- tdObject$phy
+    out <- eval(call, env)
+    if(is.null(out)){
+      invisible()
+    } else {
+      return(out)
+    }
+  }else{
+
+    lapply(seq_along(tdObject$phy), function(x){
+      env$phy <- tdObject$phy[[x]]
+      out <- eval(call, env)
+      if(is.null(out)){
+        invisible()
+      } else {
+        return(out)
+      }
+    })
   }
 }
