be1a218c851c3b398368dcfe6d3f11125641a49e
cromanpa94 <cromanpa94@gmail.com>
2020-08-10 15:04:37 -0700

    Fixing tips dropped x2 [

--- R/treedata.table.R
+++ R/treedata.table.R
@@ -68,13 +68,10 @@
     }
   }
 
-  # .phy <- ape::drop.tip(x$phy, which(!1:nrow(x$dat) %in%
-  #  unlist(.dat[, "rowid"]))) #CRP: using "rowid" instead of rowid & unlist
-
   .phy <- if (inherits(x$phy, c("phylo"))) {
-    ape::drop.tip(x$phy, which(!1:nrow(x$dat) %in% unlist(.dat[, "rowid"])))
+    ape::drop.tip(x$phy, which(!seq_len(nrow(x$dat)) %in% unlist(.dat[, "rowid"])))
   } else {
-    tr <- lapply(x$phy, ape::drop.tip, tip = which(!1:nrow(x$dat)
+    tr <- lapply(x$phy, ape::drop.tip, tip = which(!seq_len(nrow(x$dat))
     %in% unlist(.dat[, "rowid"])))
     class(tr) <- "multiPhylo"
     tr
