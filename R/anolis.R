7a36eca0198b32a5f40adbc62979060af23df51e
cromanpa94 <cromanpa94@gmail.com>
2020-09-05 07:40:05 -0700

    Dataset information and Travis for Linux

--- R/anolis.R
+++ R/anolis.R
@@ -1,6 +1,8 @@
 #' @name anolis
 #' @title Anole data
-#' @description Anole data for treedata.table functions
+#' @description Anole data for treedata.table functions. Many
+#' of the traits (e.g. awesomeness, hostility) in this dataset
+#' retrieved from treeplyr are based on random numbers.
 #' @author Luke Harmon
 #' @docType data
 #' @usage data(anolis)
